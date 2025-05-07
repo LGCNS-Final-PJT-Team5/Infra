90405a6b43cf4f40cef4148558258c34ee7ac37a
SRASONY <leeseula0123@gmail.com>
2025-05-07 17:42:55 +0900

    feat: rds 구축 및 VPC 수정(서브넷 추가)

--- vpc.tf
+++ vpc.tf
@@ -32,14 +32,25 @@ resource "aws_subnet" "private_subnet_app" {
   }
 }
 
-# Private Subnet B (RDS)
-resource "aws_subnet" "private_subnet_rds" {
+# Private Subnet B (RDS - AZ a)
+resource "aws_subnet" "private_subnet_rds_b" {
   vpc_id            = aws_vpc.main_vpc.id
-  cidr_block        = "10.0.3.0/24"
+  cidr_block        = "10.0.8.0/24"
   availability_zone = "ap-northeast-2a"
 
   tags = {
     Name = "private-subnet-B"
+  }
+}
+
+# Private Subnet C (RDS - AZ c)
+resource "aws_subnet" "private_subnet_rds_c" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "ap-northeast-2c"
+
+  tags = {
+    Name = "private-subnet-C"
   }
 }
 
@@ -107,7 +118,12 @@ resource "aws_route_table_association" "private_app_assoc" {
   route_table_id = aws_route_table.private_rt.id
 }
 
-resource "aws_route_table_association" "private_rds_assoc" {
-  subnet_id      = aws_subnet.private_subnet_rds.id
+resource "aws_route_table_association" "private_rds_b_assoc" {
+  subnet_id      = aws_subnet.private_subnet_rds_b.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_rds_c_assoc" {
+  subnet_id      = aws_subnet.private_subnet_rds_c.id
   route_table_id = aws_route_table.private_rt.id
 }
