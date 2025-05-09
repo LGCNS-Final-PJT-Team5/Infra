0ed39a7fa6eb501fa427835ad7d4dc9b69a78625
SRASONY <leeseula0123@gmail.com>
2025-05-09 11:18:04 +0900

    feat: kinesis iam role 추가

--- iam.tf
+++ iam.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 ##########################
 # EC2->SSM 접속용 IAM Role
 ##########################
@@ -25,4 +27,56 @@ resource "aws_iam_role_policy_attachment" "ssm_ec2_attach" {
 resource "aws_iam_instance_profile" "ssm_ec2_profile" {
   name = "ssm-ec2-instance-profile"
   role = aws_iam_role.ssm_ec2_role.name
+}
+
+
+
+###################################
+# IoT core -> Kinesis 전송용 IAM Role
+###################################
+
+# 1. IAM Role 
+resource "aws_iam_role" "iot_to_kinesis_role" {
+  name = "iot-to-kinesis-role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "iot.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# 2. IAM Policy (Kinesis로 Put 할 수 있는 권한)
+resource "aws_iam_policy" "iot_kinesis_policy" {
+  name = "iot-to-kinesis-policy"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "kinesis:PutRecord",
+          "kinesis:PutRecords"
+        ],
+        "Resource": [
+          "arn:aws:kinesis:ap-northeast-2:${data.aws_caller_identity.current.account_id}:stream/sensor-data-stream",
+          "arn:aws:kinesis:ap-northeast-2:${data.aws_caller_identity.current.account_id}:stream/event-data-stream"
+        ]
+      }
+    ]
+  })
+}
+
+# 3. Role에 Policy 연결
+resource "aws_iam_role_policy_attachment" "attach_iot_kinesis_policy" {
+  role       = aws_iam_role.iot_to_kinesis_role.name
+  policy_arn = aws_iam_policy.iot_kinesis_policy.arn
 }
